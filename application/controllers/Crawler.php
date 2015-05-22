927e031bfe13ba1b74f1ddbbeaf3a5599ff198ac
Le Vinh Phu <lvphu609@gmail.com>
2015-05-22 15:31:50 +0700

    asdasd

--- application/controllers/Crawler.php
+++ application/controllers/Crawler.php
@@ -157,27 +157,6 @@ class Crawler extends CI_Controller
         shell_exec('cd robot/ && casperjs crawler.js');
     }
 
-    /**
-    *
-    * Convert an object to an array
-    *
-    * @param    object  $object The object to convert
-    * @reeturn      array
-    *
-    */
-    function objectToArray( $object )
-    {
-        if( !is_object( $object ) && !is_array( $object ) )
-        {
-            return $object;
-        }
-        if( is_object( $object ) )
-        {
-            $object = get_object_vars( $object );
-        }
-        return array_map( 'objectToArray', $object );
-    }
-
     public function test(){
         $isExist = $this->cimongo->get_where('category',array('alias' => "dien_thoai_may_tinh_bang"));
         $a = $isExist->result_array();
